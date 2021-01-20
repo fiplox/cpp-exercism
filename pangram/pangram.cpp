a5c602c51baabd899e896fe683ee54aa6565f7a8
Volodymyr Patuta <6977238-fiplox@users.noreply.gitlab.com>
2021-01-20 08:25:04 +0100

    next iteration

--- pangram/pangram.cpp
+++ pangram/pangram.cpp
@@ -1,18 +1,17 @@
 #include "pangram.h"
 #include <map>
-#include <algorithm>
-#include <ctype.h>
+#include <cctype>
 #include <unordered_set>
 
 namespace pangram {
-	bool is_pangram(std::string s) 
+	bool is_pangram(const std::string s) 
 	{
 		std::unordered_set<char> char_set;
 
 		for (char c : s){
 			c = std::tolower(c);
-			if (c > 96 && c < 123)
-				char_set.insert(std::tolower(c));
+			if (std::islower(c))
+				char_set.insert(c);
 		}
 		return char_set.size() == 26;
 	}
